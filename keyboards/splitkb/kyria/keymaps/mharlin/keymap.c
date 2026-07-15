d23f0aef8828215a0c8de061486fb39c193b8ffc
mharlin <magnus.harlin_contractor@recordedfuture.com>
2026-07-15 15:54:39 +0200

    Use one-shot right Alt on Num layer for Alt+Number combos (tmux)
    Left Alt gets composed into special chars by macOS/Ghostty; right Alt
    passes through as Meta. One-shot avoids holding two keys across hands.

--- keyboards/splitkb/kyria/keymaps/mharlin/keymap.c
+++ keyboards/splitkb/kyria/keymaps/mharlin/keymap.c
@@ -338,7 +338,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_NUM] = LAYOUT(
       KC_NO, KC_LBRC,           KC_7,              KC_8,              KC_9,              KC_RBRC,                              U_NA,              TD(TD_BASE_SWITCH),A_RING,            A_UM,              O_UM,      KC_NO,         \
-      KC_NO, KC_SCLN,           KC_4,              KC_5,              LSFT_T(KC_6),      KC_EQL,                               U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,   KC_NO,         \
+      KC_NO, KC_SCLN,           KC_4,              KC_5,              LSFT_T(KC_6),      KC_EQL,                               U_NA,              KC_LSFT,           KC_LCTL,           OSM(MOD_RALT),     KC_LGUI,   KC_NO,         \
       KC_NO, KC_GRV,            KC_1,              KC_2,              KC_3,              KC_BSLS,  KC_NO,KC_NO, KC_NO,KC_NO,   U_NA,              TD(TD_NUM_SWITCH), TD(TD_NAV_SWITCH), KC_ALGR,           U_NA,      KC_NO,         \
                                             KC_NO, KC_DOT,            KC_0,              KC_MINS,  KC_NO, KC_NO,   U_NA,              U_NA,              U_NA, KC_NO
     ),
